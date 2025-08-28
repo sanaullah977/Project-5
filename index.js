8943e3bcb5f95def05bfef32ffb6a4fdb018aabc
H.M. Sana Ullah <aponhasan972@gmail.com>
2025-08-29 00:46:30 +0600

    Complete all card function and responsive TAsk

--- index.js
+++ index.js
@@ -1,16 +1,348 @@
-document.getElementById('heartIcon');
+
+
+// Heart Icon & Counter proccessing section
+
+document.getElementById('heartIcon1');
 document.getElementById('heartCounter')
 
 let count = 0;
 
-heartIcon.addEventListener('click' , function() {
+heartIcon1.addEventListener('click' , function() {
     count ++ ;
     
     heartCounter.textContent = count ;
 });
 
-// document.getElementById('copyBtn');
-// document.getElementById('copyCounter')
+document.getElementById('heartIcon2');
+document.getElementById('heartCounter')
+
+
+
+heartIcon2.addEventListener('click' , function() {
+    count ++ ;
+    
+    heartCounter.textContent = count ;
+});
+
+document.getElementById('heartIcon3');
+document.getElementById('heartCounter')
+
+
+heartIcon3.addEventListener('click' , function() {
+    count ++ ;
+    
+    heartCounter.textContent = count ;
+});
+
+document.getElementById('heartIcon4');
+document.getElementById('heartCounter')
+
+
+heartIcon4.addEventListener('click' , function() {
+    count ++ ;
+    
+    heartCounter.textContent = count ;
+});
+
+document.getElementById('heartIcon5');
+document.getElementById('heartCounter')
+
+
+heartIcon5.addEventListener('click' , function() {
+    count ++ ;
+    
+    heartCounter.textContent = count ;
+});
+
+document.getElementById('heartIcon6');
+document.getElementById('heartCounter')
+
+
+heartIcon6.addEventListener('click' , function() {
+    count ++ ;
+    
+    heartCounter.textContent = count ;
+});
+
+document.getElementById('heartIcon7');
+document.getElementById('heartCounter')
+
+
+
+heartIcon7.addEventListener('click' , function() {
+    count ++ ;
+    
+    heartCounter.textContent = count ;
+});
+
+document.getElementById('heartIcon8');
+document.getElementById('heartCounter')
+
+
+
+heartIcon8.addEventListener('click' , function() {
+    count ++ ;
+    
+    heartCounter.textContent = count ;
+});
+
+document.getElementById('heartIcon9');
+document.getElementById('heartCounter')
+
+
+
+heartIcon9.addEventListener('click' , function() {
+    count ++ ;
+    
+    heartCounter.textContent = count ;
+});
+
+
+// Copy Button & Counter Proccessing Section
+
+
+document.getElementById('copyBtn1');
+document.getElementById('copyCounter')
+
+let copycount= 0;
+
+copyBtn1.addEventListener('click' , function() {
+    copycount ++ ;
+    
+    copyCounter.textContent = copycount;
+});
+
+
+
+document.getElementById('copyBtn2');
+document.getElementById('copyCounter')
+
+
+copyBtn2.addEventListener('click' , function() {
+    copycount ++ ;
+    
+    copyCounter.textContent = copycount;
+});
+
+document.getElementById('copyBtn3');
+document.getElementById('copyCounter')
+
+
+copyBtn3.addEventListener('click' , function() {
+    copycount ++ ;
+    
+    copyCounter.textContent = copycount;
+});
+
+document.getElementById('copyBtn4');
+document.getElementById('copyCounter')
+
+
+copyBtn4.addEventListener('click' , function() {
+    copycount ++ ;
+    
+    copyCounter.textContent = copycount;
+});
+
+document.getElementById('copyBtn5');
+document.getElementById('copyCounter')
+
+
+copyBtn5.addEventListener('click' , function() {
+    copycount ++ ;
+    
+    copyCounter.textContent = copycount;
+});
+
+document.getElementById('copyBtn6');
+document.getElementById('copyCounter')
+
+
+copyBtn6.addEventListener('click' , function() {
+    copycount ++ ;
+    
+    copyCounter.textContent = copycount;
+});
+
+document.getElementById('copyBtn7');
+document.getElementById('copyCounter')
+
+
+copyBtn7.addEventListener('click' , function() {
+    copycount ++ ;
+    
+    copyCounter.textContent = copycount;
+});
+
+document.getElementById('copyBtn8');
+document.getElementById('copyCounter')
+
+
+copyBtn8.addEventListener('click' , function() {
+    copycount ++ ;
+    
+    copyCounter.textContent = copycount;
+});
+
+document.getElementById('copyBtn9');
+document.getElementById('copyCounter')
+
+
+copyBtn9.addEventListener('click' , function() {
+    copycount ++ ;
+    
+    copyCounter.textContent = copycount;
+});
+
+
+
+
+// Call & Coin proccessing section
+
+
+document.getElementById('callBtn1');
+document.getElementById('coinCounter');
+
+let currentCoin = parseInt(coinCounter.innerText);
+callBtn1.addEventListener('click', function(){
+    if( currentCoin >= 20 ){
+        currentCoin -=20;
+        coinCounter.innerText = currentCoin;
+        alert("কল করা হচ্ছে ");
+    }
+    else(
+        alert("আপনার কাছে পর্যাপ্ত পরিমাণ কয়েন নেই।")
+    )
+    
+});
+
+document.getElementById('callBtn2');
+document.getElementById('coinCounter');
+
+
+callBtn2.addEventListener('click', function(){
+    if( currentCoin >= 20 ){
+        currentCoin -=20;
+        coinCounter.innerText = currentCoin;
+        alert("কল করা হচ্ছে ");
+    }
+    else(
+        alert("আপনার কাছে পর্যাপ্ত পরিমাণ কয়েন নেই।")
+    )
+    
+});
+
+document.getElementById('callBtn3');
+document.getElementById('coinCounter');
+
+
+callBtn3.addEventListener('click', function(){
+    if( currentCoin >= 20 ){
+        currentCoin -=20;
+        coinCounter.innerText = currentCoin;
+        alert("কল করা হচ্ছে ");
+    }
+    else(
+        alert("আপনার কাছে পর্যাপ্ত পরিমাণ কয়েন নেই।")
+    )
+    
+});
+
+document.getElementById('callBtn4');
+document.getElementById('coinCounter');
+
+
+callBtn4.addEventListener('click', function(){
+    if( currentCoin >= 20 ){
+        currentCoin -=20;
+        coinCounter.innerText = currentCoin;
+        alert("কল করা হচ্ছে ");
+    }
+    else(
+        alert("আপনার কাছে পর্যাপ্ত পরিমাণ কয়েন নেই।")
+    )
+    
+});
+
+document.getElementById('callBtn5');
+document.getElementById('coinCounter');
+
+
+callBtn5.addEventListener('click', function(){
+    if( currentCoin >= 20 ){
+        currentCoin -=20;
+        coinCounter.innerText = currentCoin;
+        alert("কল করা হচ্ছে ");
+    }
+    else(
+        alert("আপনার কাছে পর্যাপ্ত পরিমাণ কয়েন নেই।")
+    )
+    
+});
+
+document.getElementById('callBtn6');
+document.getElementById('coinCounter');
+
+
+callBtn6.addEventListener('click', function(){
+    if( currentCoin >= 20 ){
+        currentCoin -=20;
+        coinCounter.innerText = currentCoin;
+        alert("কল করা হচ্ছে ");
+    }
+    else(
+        alert("আপনার কাছে পর্যাপ্ত পরিমাণ কয়েন নেই।")
+    )
+    
+});
+
+document.getElementById('callBtn7');
+document.getElementById('coinCounter');
+
+
+callBtn7.addEventListener('click', function(){
+    if( currentCoin >= 20 ){
+        currentCoin -=20;
+        coinCounter.innerText = currentCoin;
+        alert("কল করা হচ্ছে ");
+    }
+    else(
+        alert("আপনার কাছে পর্যাপ্ত পরিমাণ কয়েন নেই।")
+    )
+    
+});
+
+document.getElementById('callBtn8');
+document.getElementById('coinCounter');
+
+
+callBtn8.addEventListener('click', function(){
+    if( currentCoin >= 20 ){
+        currentCoin -=20;
+        coinCounter.innerText = currentCoin;
+        alert("কল করা হচ্ছে ");
+    }
+    else(
+        alert("আপনার কাছে পর্যাপ্ত পরিমাণ কয়েন নেই।")
+    )
+    
+});
+
+document.getElementById('callBtn9');
+document.getElementById('coinCounter');
+
+
+callBtn9.addEventListener('click', function(){
+    if( currentCoin >= 20 ){
+        currentCoin -=20;
+        coinCounter.innerText = currentCoin;
+        alert("163 কল করা হচ্ছে ");
+    }
+    else(
+        alert("আপনার কাছে পর্যাপ্ত পরিমাণ কয়েন নেই।")
+    )
+    
+});
 
 
 
